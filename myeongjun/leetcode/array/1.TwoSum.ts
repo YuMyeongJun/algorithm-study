eb3da9e0e57c3d4acb61db85d3e2aeb13032657b
유명준 <mj.yu@ilts.co.kr>
2021-11-17 11:56:08 +0900

    변수명 수정

--- myeongjun/leetcode/array/1.TwoSum.ts
+++ myeongjun/leetcode/array/1.TwoSum.ts
@@ -1,10 +1,10 @@
-function twoSum(numbers: number[], target: number): number[] {
+function twoSum(nums: number[], target: number): number[] {
 
     let output: number[] = [];
 
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
                 output.push(i, j);
                 return output;
             }
